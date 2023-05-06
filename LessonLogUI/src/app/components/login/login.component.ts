787ae14d57d1cfd34a14eaaf3c900dc05a878394
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-05-06 15:48:51 +0200

    add login validation

--- LessonLogUI/src/app/components/login/login.component.ts
+++ LessonLogUI/src/app/components/login/login.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      console.log("Form is not valid");
+      this.validateAllFormFields(this.loginForm)
+      // add toast message
+      alert("Your form is invalid");
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    })
+  }
 }
