9a8aa32bda3f8ce251d4558c2b95c86b1b99e0f0
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-05-07 12:52:48 +0200

    add register user action logic

--- LessonLogAPI/LessonLogAPI/Controllers/UserController.cs
+++ LessonLogAPI/LessonLogAPI/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LessonLogAPI.Context;
+using LessonLogAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LessonLogAPI.Controllers
 {
@@ -7,5 +9,49 @@ namespace LessonLogAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly AppDbContext _dbContext;
+
+        public UserController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if(userObj == null)
+            {
+                return BadRequest();
+            }
+
+            var user = await _dbContext.Users
+                .FirstOrDefaultAsync(x => x.Email == userObj.Email && x.Password == userObj.Password);
+
+            if(user == null)
+            {
+                return NotFound(new {Message = "User Not Found!"});
+            }
+
+            return Ok(new
+            {
+                Message = "Login Success!"
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj== null)
+            {
+                return BadRequest();
+            }
+
+            await _dbContext.Users.AddAsync(userObj);
+            await _dbContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registered!"
+            });
+        }
     }
 }
