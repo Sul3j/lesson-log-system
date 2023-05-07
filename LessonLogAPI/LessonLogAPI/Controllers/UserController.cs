2ed8d5df8849f564c26abf5835198ec8a5463681
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-05-07 12:37:05 +0200

    add login user action logic

---
+++ LessonLogAPI/LessonLogAPI/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LessonLogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
