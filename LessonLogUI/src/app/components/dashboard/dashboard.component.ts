539794a1a487ddcfd180ae18d6191adba6323d70
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-05-09 19:59:36 +0200

    add get all users in the dashboard

--- LessonLogUI/src/app/components/dashboard/dashboard.component.ts
+++ LessonLogUI/src/app/components/dashboard/dashboard.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
 import {ToastrService} from "ngx-toastr";
+import {ApiService} from "../../services/api.service";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-  constructor(private auth: AuthService, private toastr: ToastrService) {}
+export class DashboardComponent implements OnInit {
+  public users: any = [];
+
+  constructor(private auth: AuthService, private toastr: ToastrService, private api: ApiService) {}
+
   logout() {
     this.auth.logout();
     this.toastr.success('Logged out!')
+  }
+
+  ngOnInit(): void {
+    this.api.getUsers().subscribe(res => {
+      this.users = res;
+    })
   }
 }
