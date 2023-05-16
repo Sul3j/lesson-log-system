c23faee8b70f9e515b9391f7bb9fd1569d43f767
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-05-16 21:12:23 +0200

    add call to reset password endpoint

--- LessonLogUI/src/app/components/reset/reset.component.ts
+++ LessonLogUI/src/app/components/reset/reset.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {ResetPassword} from "../../models/reset-password.model";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ConfirmPasswordValidator} from "../../helpers/confirm-password.validator";
+import {ActivatedRoute, Router} from "@angular/router";
+import ValidateForm from "../../helpers/validateform";
+import {ResetPasswordService} from "../../services/reset-password.service";
+import {ToastrModule, ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-reset',
@@ -14,7 +18,12 @@ export class ResetComponent implements OnInit {
   emailToken!: string;
   resetPasswordObj = new ResetPassword();
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private resetService: ResetPasswordService,
+    private toastr: ToastrService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.resetPasswordForm = this.fb.group({
@@ -22,10 +31,33 @@ export class ResetComponent implements OnInit {
       confirmPassword: [null, Validators.required]
     },{
       validator: ConfirmPasswordValidator("password", "confirmPassword")
+    });
+
+    this.activatedRoute.queryParams.subscribe(val => {
+      this.emailToReset = val['email'];
+      let uriToken = val['code'];
+      this.emailToken = uriToken.replace(/ /g, '+');
     })
   }
 
   submit() {
+    if(this.resetPasswordForm.valid) {
+      this.resetPasswordObj.email = this.emailToReset;
+      this.resetPasswordObj.newPassword = this.resetPasswordForm.value.password;
+      this.resetPasswordObj.confirmPassword = this.resetPasswordForm.value.confirmPassword;
+      this.resetPasswordObj.emailToken = this.emailToken;
 
+      this.resetService.resetPassword(this.resetPasswordObj)
+        .subscribe({
+          next: (res) => {
+            this.toastr.success("Password reset successfully!");
+            this.router.navigate(['/']);
+          }, error: (err) => {
+            this.toastr.error("Something went wrong!");
+          }
+        })
+    } else {
+      ValidateForm.validateAllFormFields(this.resetPasswordForm);
+    }
   }
 }
