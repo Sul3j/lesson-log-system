64c4c01530181f532caf81e8f580f312a5cbd1ab
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-10-17 12:11:14 +0200

    add deleteStudent and addStudent method

--- LessonLogUI/src/app/services/students.service.ts
+++ LessonLogUI/src/app/services/students.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Subject} from "rxjs";
+import {Subject, tap} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {UrlService} from "./url.service";
 import {Pagination} from "../models/pagination.model";
@@ -19,5 +19,25 @@ export class StudentsService {
 
   getStudents(body: Pagination) {
     return this.http.post<any>(`${this.urlService.url}/Student/pagination`, body);
+  }
+
+  addStudent(userId: number) {
+    return this.http
+      .post(`${this.urlService.url}/STUDENT/add`, { "userId": userId })
+      .pipe(
+        tap(() => {
+          this._refreshNedeed.next();
+        })
+      );
+  }
+
+  deleteStudent(studentId: number) {
+    return this.http
+      .delete(`${this.urlService.url}/STUDENT/${studentId}`)
+      .pipe(
+        tap(() => {
+          this._refreshNedeed.next();
+        })
+      );
   }
 }
