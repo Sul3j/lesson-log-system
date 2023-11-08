e083c0f3de85da606a6a9b071d515c4d686ddfdb
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-11-08 12:00:10 +0100

    add all clasroom pagination methods

--- LessonLogUI/src/app/components/classrooms/classrooms.component.ts
+++ LessonLogUI/src/app/components/classrooms/classrooms.component.ts
@@ -5,6 +5,7 @@ import {ToastrService} from "ngx-toastr";
 import {Pagination} from "../../models/pagination.model";
 import {Classroom} from "../../models/classroom.model";
 import {ResponseModel} from "../../models/response.model";
+import {ClassroomDto} from "../../models/classroom.dto";
 
 @Component({
   selector: 'app-classrooms',
@@ -15,6 +16,7 @@ export class ClassroomsComponent implements OnInit{
   public classrooms: Array<Classroom> = new Array<Classroom>();
   public paginationModel: Pagination = new Pagination();
   public response: ResponseModel<Classroom> = new ResponseModel<Classroom>();
+  public classroomValue: ClassroomDto = new ClassroomDto();
 
   constructor(private classroomService: ClassroomsService,
               private helperService: HelperService,
@@ -32,6 +34,35 @@ export class ClassroomsComponent implements OnInit{
       .subscribe(() => {
         this.getAllClassrooms();
       })
+    this.getAllClassrooms();
+  }
+
+  searchClass(e: any) {
+    this.paginationModel = this.helperService.setPaginationFilter(e);
+    this.getAllClassrooms();
+  }
+
+  nextPage(): void {
+    this.paginationModel.page = this.helperService.nextPage(this.response.totalPages, this.paginationModel.page);
+    this.getAllClassrooms();
+  }
+
+  itemsPerPage(e: any) {
+    this.paginationModel.pageSize = parseInt(e.target.value);
+    this.getAllClassrooms();
+  }
+
+  previousPage(): void {
+    this.paginationModel.page = this.helperService.previousPage(this.paginationModel.page);
+    this.getAllClassrooms();
+  }
+
+  createRange(number: number) {
+    return this.helperService.createRange(number);
+  }
+
+  changePage(e: any) {
+    this.paginationModel.page = parseInt(e.target.value);
     this.getAllClassrooms();
   }
 }
