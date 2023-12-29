d03b571ec2ef0317806cb89621df0ea456cad1df
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-12-29 12:43:20 +0100

    add student name to nav menu

--- LessonLogUI/src/app/components/student/student-main-nav/student-main-nav.component.ts
+++ LessonLogUI/src/app/components/student/student-main-nav/student-main-nav.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import {AuthService} from "../../../services/auth.service";
 import {ToastrService} from "ngx-toastr";
+import {UsersService} from "../../../services/users.service";
+import {User} from "../../../models/user.model";
+import {jwtDecode} from "jwt-decode";
 
 @Component({
   selector: 'app-student-main-nav',
   templateUrl: './student-main-nav.component.html',
   styleUrls: ['./student-main-nav.component.scss']
 })
-export class StudentMainNavComponent {
+export class StudentMainNavComponent implements OnInit {
+
+  public user: User = new User();
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -18,10 +23,23 @@ export class StudentMainNavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, private auth: AuthService, private toastr: ToastrService) {}
+  constructor(private breakpointObserver: BreakpointObserver, private auth: AuthService, private toastr: ToastrService, private userService: UsersService) {}
 
   logout() {
     this.auth.logout();
     this.toastr.success("Logged out!", "Success");
+  }
+
+  ngOnInit(): void {
+    this.getUserByEmail();
+  }
+
+  getUserByEmail() {
+    const localStorage = window.localStorage;
+    const token = localStorage.getItem("token") as string;
+    const decodeToken = jwtDecode(token) as any;
+    this.userService.getUserById(decodeToken.unique_name).subscribe((res: any) => {
+      this.user = res;
+    })
   }
 }
